6f11f6b1ed6d4786e75b8c5cdddc7d58761b662e
StarckmannP <hikoyyv3@gmail.com>
2023-11-20 16:30:36 +0100

    Création de CoursType

---
+++ src/Form/CoursType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Cours;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+class CoursType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('libelle')
+            ->add('ageMini')
+            ->add('heureDebut')
+            ->add('heureFin')
+            ->add('nbPlaces')
+            ->add('ageMaxi')
+            ->add('idJour')
+            ->add('idTypeCours')
+            ->add('idProfesseur')
+            ->add('idTypeInstrument')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Cours::class,
+        ]);
+    }
+}
