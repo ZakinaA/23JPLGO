2e8914b9e26becdb1cd6fe5588cd4307291ecd67
StarckmannP <hikoyyv3@gmail.com>
2023-12-20 09:36:17 +0100

    Ajout des form accessoire

---
+++ src/Form/AccessoireType.php
@@ -0,0 +1,33 @@
+<?php
+
+// src/Form/AccessoireType.php
+
+namespace App\Form;
+
+use App\Entity\Accessoire;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AccessoireType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('libelle', TextType::class, ['attr' => ['class' => 'form-control']])
+
+            ->add('save', SubmitType::class, ['label' => 'Créer']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Accessoire::class,
+        ]);
+    }
+}
+
+
